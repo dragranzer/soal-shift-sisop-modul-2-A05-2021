63142dfbda4800ffddefa721fcc39f792fc08fbb
ivannizar <71221969+dragranzer@users.noreply.github.com>
2021-04-15 21:51:43 +0700

    Update soal3.c
    mulai dapat pencerahan

--- soal3/soal3.c
+++ soal3/soal3.c
@@ -1,3 +1,12 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <syslog.h>
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -7,93 +16,74 @@
 #include <wait.h>
 
 void ConstructTimeNow(char*);
+void ConstructTimeNow2(char*);
 void ConstructDownloadLink(char* ,char*, char*);
-
-int main(){
+char format_name[50];
+int main() {
     pid_t child_id;
     int status;
-    char format_name[50];
+    
     child_id = fork();
 
     if (child_id < 0) {
         exit(EXIT_FAILURE); // Jika gagal membuat proses baru, program akan berhenti
     }
-
+    
     if (child_id == 0) {
         // this is child
+        while(1){
+            pid_t child_id1;
 
-        pid_t pid, sid;        // Variabel untuk menyimpan PID
+            child_id1 = fork();
+
+            if (child_id1 < 0) {
+                exit(EXIT_FAILURE); // Jika gagal membuat proses baru, program akan berhenti
+            }
             
-        pid = fork();     // Menyimpan PID dari Child Process
+            if (child_id1 == 0) {
+                // this is child
+                ConstructTimeNow(format_name);
+                char *argv[] = {"mkdir", "-p", format_name, NULL};
+                execv("/bin/mkdir", argv);
+            } else {
+                // this is parent
 
-        /* Keluar saat fork gagal
-        * (nilai variabel pid < 0) */
-        if (pid < 0) {
-            exit(EXIT_FAILURE);
+                sleep(40);
+            }
         }
-
-        /* Keluar saat fork berhasil
-        * (nilai variabel pid adalah PID dari child process) */
-        if (pid > 0) {
-            exit(EXIT_SUCCESS);
-        }
-
-        umask(0);
-
-        sid = setsid();
-        if (sid < 0) {
-            exit(EXIT_FAILURE);
-        }
-
-        if ((chdir("/")) < 0) {
-            exit(EXIT_FAILURE);
-        }
-
-        close(STDIN_FILENO);
-        close(STDOUT_FILENO);
-        close(STDERR_FILENO);
-
-        while (1) {
-            // Tulis program kalian di sini
-                
-            ConstructTimeNow(format_name);   
-            char *argv[] = {"mkdir", "-p", format_name, NULL};
-            execv("/bin/mkdir", argv);
-            sleep(10);
-        }
+        
     } else {
         // this is parent
-        while ((wait(&status)) > 0);
+        sleep(2);
         int j=0;
         char path[1000];
-            
+        ConstructTimeNow2(format_name);
         for(j=0;j<10;j++){
-            pid_t child_id;
-            int status1;
+            pid_t child_id2;
 
-            child_id = fork();
+            child_id2 = fork();
 
-            if (child_id < 0) {
+            if (child_id2 < 0) {
                 exit(EXIT_FAILURE); // Jika gagal membuat proses baru, program akan berhenti
             }
 
-            if (child_id == 0) {
+            if (child_id2 == 0) {
                 // this is child
-                while ((wait(&status)) > 0);
                 char size[5],download_link[1000],name[50];
                 ConstructDownloadLink(size, download_link, name);
                 sprintf(path, "%s/%s", format_name,name);
+                printf("%s\n",format_name);
                 char *argv[] = {"wget", "--no-check-certificate", "-q", download_link, "-O", path, NULL};
                 execv("/bin/wget", argv);
+                
             } else {
                 // this is parent
-                
+                sleep(5);
             }
         }
-            
+        
+
     }
- 
-    return 0;
 }
 
 void ConstructTimeNow(char *format_name){
@@ -108,10 +98,25 @@ void ConstructTimeNow(char *format_name){
     day = local->tm_mday;            // get day of month (1 to 31)
     month = local->tm_mon + 1;       // get month of year (0 to 11)
     year = local->tm_year + 1900;    // get year since 1900
-    sprintf(format_name,"%d-%02d-%02d_%02d:%02d:%02d\n", year, month, day, hours, minutes, seconds);
+    sprintf(format_name,"%d-%02d-%02d_%02d:%02d:%02d", year, month, day, hours, minutes, seconds);
 }
 void ConstructDownloadLink(char *size ,char *link,char *name){
     sprintf(size,"%d",(int)time(NULL)%1000+50); 
     ConstructTimeNow(name);
     sprintf(link,"https://picsum.photos/id/2/%s/%s",size,size);
+}
+
+void ConstructTimeNow2(char *format_name){
+    int hours, minutes, seconds, day, month, year;
+    time_t now;
+    time(&now);
+    struct tm *local = localtime(&now);
+    hours = local->tm_hour;          // get hours since midnight (0-23)
+    minutes = local->tm_min;         // get minutes passed after the hour (0-59)
+    seconds = local->tm_sec;         // get seconds passed after minute (0-59)
+            
+    day = local->tm_mday;            // get day of month (1 to 31)
+    month = local->tm_mon + 1;       // get month of year (0 to 11)
+    year = local->tm_year + 1900;    // get year since 1900
+    sprintf(format_name,"%d-%02d-%02d_%02d:%02d:%02d", year, month, day, hours, minutes, seconds-2);
 }
