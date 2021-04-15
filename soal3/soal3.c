53c296e4d53cfe423847145dfac265951642b193
ivannizar <71221969+dragranzer@users.noreply.github.com>
2021-04-15 23:52:38 +0700

    Update soal3.c
    ada bug dikit

--- soal3/soal3.c
+++ soal3/soal3.c
@@ -17,8 +17,9 @@
 
 void ConstructTimeNow(char*);
 void ConstructTimeNow2(char*);
+void ConstructTimeNowSpecial(char*);
 void ConstructDownloadLink(char* ,char*, char*);
-char format_name[50];
+char format_name[50],temp[50];
 int main() {
     pid_t child_id;
     int status;
@@ -54,35 +55,44 @@ int main() {
         
     } else {
         // this is parent
-        sleep(2);
-        int j=0;
-        char path[1000];
-        ConstructTimeNow2(format_name);
-        for(j=0;j<10;j++){
-            pid_t child_id2;
-
-            child_id2 = fork();
-
-            if (child_id2 < 0) {
-                exit(EXIT_FAILURE); // Jika gagal membuat proses baru, program akan berhenti
-            }
-
-            if (child_id2 == 0) {
-                // this is child
-                char size[5],download_link[1000],name[50];
-                ConstructDownloadLink(size, download_link, name);
-                sprintf(path, "%s/%s", format_name,name);
-                printf("%s\n",format_name);
-                char *argv[] = {"wget", "--no-check-certificate", "-q", download_link, "-O", path, NULL};
-                execv("/bin/wget", argv);
+        sleep(0.01);
+        int k=0;
+        while(1){
+            
+            int j=0;
+            char path[1000];
+            if(k==0)ConstructTimeNow(format_name);
+            else sprintf(format_name, "%s", temp);
+            // else ConstructTimeNowSpecial(format_name, k);
+            
+            for(j=0;j<10;j++){
                 
-            } else {
-                // this is parent
-                sleep(5);
-            }
-        }
-        
+                if(j==((40-10*k)%60)/5)ConstructTimeNow(temp);
+                //printf("%s\n",temp);
+                pid_t child_id2;
 
+                child_id2 = fork();
+
+                if (child_id2 < 0) {
+                    exit(EXIT_FAILURE); // Jika gagal membuat proses baru, program akan berhenti
+                }
+
+                if (child_id2 == 0) {
+                    // this is child
+                    char size[5],download_link[1000],name[50];
+                    ConstructDownloadLink(size, download_link, name);
+                    sprintf(path, "%s/%s", format_name,name);
+                    char *argv[] = {"wget", "--no-check-certificate", "-q", download_link, "-O", path, NULL};
+                    execv("/bin/wget", argv);
+                    
+                } else {
+                    // this is parent
+                    sleep(5);
+                }
+            }
+            k++;
+            if(k==5)k=1;
+        }
     }
 }
 
@@ -119,4 +129,19 @@ void ConstructTimeNow2(char *format_name){
     month = local->tm_mon + 1;       // get month of year (0 to 11)
     year = local->tm_year + 1900;    // get year since 1900
     sprintf(format_name,"%d-%02d-%02d_%02d:%02d:%02d", year, month, day, hours, minutes, seconds-2);
+}
+
+void ConstructTimeNowSpecial(char *format_name){
+    int hours, minutes, seconds, day, month, year;
+    time_t now;
+    time(&now);
+    struct tm *local = localtime(&now);
+    hours = local->tm_hour;          // get hours since midnight (0-23)
+    minutes = local->tm_min;         // get minutes passed after the hour (0-59)
+    seconds = local->tm_sec;         // get seconds passed after minute (0-59)
+            
+    day = local->tm_mday;            // get day of month (1 to 31)
+    month = local->tm_mon + 1;       // get month of year (0 to 11)
+    year = local->tm_year + 1900;    // get year since 1900
+    sprintf(format_name,"%d-%02d-%02d_%02d:%02d:%02d", year, month, day, hours, minutes, seconds);
 }
