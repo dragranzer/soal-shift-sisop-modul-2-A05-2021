987f8ef887c89e03739439ae90d7ca3d964c9cc4
ivannizar <71221969+dragranzer@users.noreply.github.com>
2021-04-16 22:08:08 +0700

    Update soal3.c
    3d, but 3e bingoeng wkwk

--- soal3/soal3.c
+++ soal3/soal3.c
@@ -1,5 +1,6 @@
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
@@ -8,92 +9,52 @@
 #include <syslog.h>
 #include <string.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <time.h>
 #include <unistd.h>
 #include <wait.h>
+#include <ctype.h>
 
 void ConstructTimeNow(char*);
 void ConstructDownloadLink(char* ,char*, char*);
-void encrypt(char* , char*);
+void encrypt(char* , char*, int);
+void phrase_3a();
+void __makeFolder(char *);
+void phrase_3b();
+void downloadImage(char*);
+void phrase_3c(char*);
+int min(int a, int b) { return (a > b ? a : b); };
+void phrase_3d();
+
 char format_name[50],temp[50];
-int main() {
-    pid_t child_id;
-    int status;
-    
-    child_id = fork();
+int main(char *argv[]) {
+    pid_t pid, sid;        // Variabel untuk menyimpan PID
 
-    if (child_id < 0) {
-        exit(EXIT_FAILURE); // Jika gagal membuat proses baru, program akan berhenti
+    pid = fork();     // Menyimpan PID dari Child Process
+
+    /* Keluar saat fork gagal
+    * (nilai variabel pid < 0) */
+    if (pid < 0) {
+      exit(EXIT_FAILURE);
     }
-    
-    if (child_id == 0) {
-        // this is child
-        while(1){
-            pid_t child_id1;
 
-            child_id1 = fork();
+    /* Keluar saat fork berhasil
+    * (nilai variabel pid adalah PID dari child process) */
+    if (pid > 0) {
+      exit(EXIT_SUCCESS);
+    }
 
-            if (child_id1 < 0) {
-                exit(EXIT_FAILURE); // Jika gagal membuat proses baru, program akan berhenti
-            }
-            
-            if (child_id1 == 0) {
-                // this is child
-                ConstructTimeNow(format_name);
-                char *argv[] = {"mkdir", "-p", format_name, NULL};
-                execv("/bin/mkdir", argv);
-            } else {
-                // this is parent
+    umask(0);
 
-                sleep(40);
-            }
-        }
-        
-    } else {
-        // this is parent
-        sleep(0.01);
-        int k=0;
-        while(1){
-            
-            int j=0;
-            char path[1000];
-            if(k==0)ConstructTimeNow(format_name);
-            else sprintf(format_name, "%s", temp);
-            // else ConstructTimeNowSpecial(format_name, k);
-            
-            for(j=0;j<10;j++){
-                
-                if(j==((40-10*k)%60)/5)ConstructTimeNow(temp);
-                //printf("%s\n",temp);
-                pid_t child_id2;
-
-                child_id2 = fork();
-
-                if (child_id2 < 0) {
-                    exit(EXIT_FAILURE); // Jika gagal membuat proses baru, program akan berhenti
-                }
-
-                if (child_id2 == 0) {
-                    // this is child
-                    char size[5],download_link[1000],name[50];
-                    ConstructDownloadLink(size, download_link, name);
-                    sprintf(path, "%s/%s", format_name,name);
-                    char *argv[] = {"wget", "--no-check-certificate", "-q", download_link, "-O", path, NULL};
-                    execv("/bin/wget", argv);
-                    
-                } else {
-                    // this is parent
-                    sleep(5);
-                }
-            }
-            char notif[50]="Download Success", encrypted[50];
-            encrypt(notif,encrypted);
-            k++;
-            if(k==5)k=1;
-        }
+    sid = setsid();
+    if (sid < 0) {
+      exit(EXIT_FAILURE);
+    }
+    phrase_3d();
+    while (1) {
+      phrase_3a();
+      sleep(5);
     }
 }
 
@@ -114,26 +75,142 @@ void ConstructTimeNow(char *format_name){
 void ConstructDownloadLink(char *size ,char *link,char *name){
     sprintf(size,"%d",(int)time(NULL)%1000+50); 
     ConstructTimeNow(name);
-    sprintf(link,"https://picsum.photos/id/2/%s/%s",size,size);
+    sprintf(link,"https://picsum.photos/%s",size);
 }
 
-void encrypt(char *text, char *encrypted)
+void encrypt(char *text, char *encrypted, int len)
 {
-    char result[100] = "";
- 
-    // traverse text
-    for (int i=0;i!='\0';i++)
-    {
-        // apply transformation to each character
-        // Encrypt Uppercase letters
-        if (isupper(text[i]))
-            result[i] = ((text[i]+5-65)%26 +65);
-            
-    // Encrypt Lowercase letters
+  len = min(strlen(text), len);
+
+  // traverse text
+  for (int i = 0; i < len; i++)
+  {
+    // apply transformation to each character
+    // Encrypt Uppercase letters
+    if (isupper(text[i]))
+      encrypted[i] = ((text[i]+5-65)%26 +65); 
     else
-        result[i] = ((text[i]+5-97)%26 +97);
+      // Encrypt Lowercase letters
+      encrypted[i] = ((text[i]+5-97)%26 +97);
+  }
+  encrypted[len] = '\0';
+}
+
+void phrase_3a() {
+  pid_t child_id;
+  int flag=1;
+  char folder_name[50];
+
+  child_id = fork();
+
+  if (child_id < 0) {
+    exit(EXIT_FAILURE);
+  }
+
+  int status;
+
+  ConstructTimeNow(folder_name);
+
+  if (child_id == 0 && flag==1) {
+    while(true){
+        __makeFolder(folder_name);
+    }
+     
+  }
+  else {
+    while(wait(&status) > 0);
+    phrase_3b(folder_name);
+  }
+}
+
+void __makeFolder(char *folder_name) {
+  char *argv[] = {"mkdir", "-p", folder_name, NULL};
+  execv("/bin/mkdir", argv);
+}
+
+void phrase_3b(char *folder_name) {
+  for (int j = 0; j < 2; j++) {
+    //printf("DEBUG: donwlot gambar ke %d\n", j);
+    downloadImage(folder_name);
+    sleep(5);
+  }
+  phrase_3c(folder_name);
+}
+
+void downloadImage(char *folder_name) {
+  pid_t child_id2;
+
+  child_id2 = fork();
+
+  if (child_id2 < 0) {
+      exit(EXIT_FAILURE); // Jika gagal membuat proses baru, program akan berhenti
+  }
+
+  int status;
+
+  if (child_id2 == 0) {
+      // this is child
+      char size[5],download_link[1000],name[50];
+      ConstructDownloadLink(size, download_link, name);
+      char path[512];
+      sprintf(path, "%s/%s.jpg", folder_name,name);
+      char *argv[] = {"wget", "--no-check-certificate", "-q", download_link, "-O", path, NULL};
+      execv("/usr/bin/wget", argv);
+      
+  } else {
+    while(wait(&status) > 0);
+  }
+}
+
+void phrase_3c(char *folder_name) {
+  pid_t child_id;
+
+  child_id = fork();
+
+  if (child_id < 0) {
+    exit(EXIT_FAILURE);
+  }
+
+  int status;
+
+  if (child_id == 0) {
+    char dir[512];
+    sprintf(dir, "%s/status.txt", folder_name);
+    FILE *fptr = fopen(dir, "w");
+    if (fptr == NULL) {
+      exit(EXIT_FAILURE);
     }
 
-    sprintf(encrypted,"%s",result);
+
+    char text_to_write[50] = "Download Success";
+
+    encrypt(text_to_write, text_to_write, strlen(text_to_write));
+    //printf("DEBUG: Menulis status.txt (Enkripsi: %s)\n", text_to_write);
+
+    fprintf(fptr, "%s", text_to_write);
+
+    fclose(fptr);
+    char zippo[50];
+    sprintf(zippo, "%s.zip", folder_name);
+    char *argv[] = {"zip", "-qrm", zippo, folder_name, NULL};
+    execv("/bin/zip", argv);
+  }
+  else {
+    while(wait(&status) > 0);
+    //printf("DEBUG: masuk zip\n");
+    
+  }
 }
 
+void phrase_3d(){
+  char dir[512];
+  sprintf(dir, "Killer.sh");
+  FILE *fptr = fopen(dir, "w");
+  if (fptr == NULL) {
+    exit(EXIT_FAILURE);
+  }
+
+  char text_to_write[50] = "killall soal3\nrm Killer.sh";
+  fprintf(fptr, "%s", text_to_write);
+  fclose(fptr);
+}
